6537929406bfd06869c9348bfc0a94d43420eda2
amumu <136903859@qq.com>
2025-04-16 10:44:28 +0800

    basic chat function

--- src/main/java/org/amumu/ai/controller/OpenAiController.java
+++ src/main/java/org/amumu/ai/controller/OpenAiController.java
@@ -1,10 +1,12 @@
 package org.amumu.ai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 
 /**
@@ -16,12 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class OpenAiController  {
 
+    private final ChatClient chatClient;
+
+    public OpenAiController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
 
     @CrossOrigin
     @GetMapping(value = "/ai/generateStreamAsString", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public String generateStreamAsString(@RequestParam(value = "message", defaultValue = "讲个笑话") String message) {
-        return null;
-        // todo 从这开始
+    public Flux<String> generateStreamAsString(@RequestParam(value = "message", defaultValue = "讲个笑话") String message) {
+        Flux<String> content = this.chatClient.prompt()
+                .user(message)
+                .stream()
+                .content();
+        return content.concatWith(Flux.just("[complete]"));// 关闭SSE长链接请求
     }
 
 
