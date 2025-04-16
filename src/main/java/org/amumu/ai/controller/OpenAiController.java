d837d004a1716a7d5ed9e0f0ea31f0de64be1c70
amumu <136903859@qq.com>
2025-04-16 10:50:22 +0800

    Preset Roles function

--- src/main/java/org/amumu/ai/controller/OpenAiController.java
+++ src/main/java/org/amumu/ai/controller/OpenAiController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.time.LocalDate;
+
 
 /**
  * @author xushu
@@ -21,7 +23,13 @@ public class OpenAiController  {
     private final ChatClient chatClient;
 
     public OpenAiController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClient = chatClientBuilder.defaultSystem(
+			   """
+					    您是“Amumu”航空公司的客户聊天支持代理。请以友好、乐于助人且愉快的方式来回复。
+					    您正在通过在线聊天系统与客户互动。
+					    请讲中文。
+					    今天的日期是 {current_date}.
+				   	""").build();
     }
 
     @CrossOrigin
@@ -29,9 +37,10 @@ public class OpenAiController  {
     public Flux<String> generateStreamAsString(@RequestParam(value = "message", defaultValue = "讲个笑话") String message) {
         Flux<String> content = this.chatClient.prompt()
                 .user(message)
+                .system(promptSystemSpec -> promptSystemSpec.param("current_date", LocalDate.now().toString()))
                 .stream()
                 .content();
-        return content.concatWith(Flux.just("[complete]"));// 关闭SSE长链接请求
+        return content.concatWith(Flux.just("[complete]"));
     }
 
 
