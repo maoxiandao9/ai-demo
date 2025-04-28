87502f7f4f53f291d8968ac84a16a9f763905b3f
amumu <136903859@qq.com>
2025-04-28 22:32:46 +0800

    add mcp server of weather

--- flight-booking/src/main/java/org/amumu/ai/controller/OpenAiController.java
+++ flight-booking/src/main/java/org/amumu/ai/controller/OpenAiController.java
@@ -1,11 +1,13 @@
 package org.amumu.ai.controller;
 
+import org.amumu.ai.services.BookingTools;
 import org.amumu.ai.services.LoggingAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +32,7 @@ public class OpenAiController  {
 
     private final ChatClient chatClient;
 
-    public OpenAiController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
+    public OpenAiController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore, BookingTools bookingTools, ToolCallbackProvider mcpTools) {
         this.chatClient = chatClientBuilder.defaultSystem(
 			   """
 					    您是“Amumu”航空公司的客户聊天支持代理。请以友好、乐于助人且愉快的方式来回复。
@@ -38,15 +40,17 @@ public class OpenAiController  {
                         在提供有关预订或取消预订的信息之前，您必须始终
                         从用户处获取以下信息：预订号、客户姓名。
                         在询问用户之前，请检查消息历史记录以获取此信息。
-                        在更改或退订之前，请先获取预定信息。
-                        获取到信息之后，询问用户是否确认更改或者退订，用户明确确定之后才可进行更改或退订，在没有得到用户同意之前不可进行操作。
                         请讲中文。
                         今天的日期是 {current_date}.
+                        在更改或退订function-call前，请先获取预订信息并且一定要等用户回复"确定"之后才进行更改或退订的function-call。 
 				   	""").defaultAdvisors(
                                new PromptChatMemoryAdvisor(chatMemory),
                                new LoggingAdvisor(),
-                               new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().build())
-                    ).defaultFunctions("cancelBooking","getBookingDetails").build();
+                               new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().build()))
+                .defaultTools(bookingTools)
+                .defaultTools(mcpTools)
+//                .defaultFunctions("cancelBooking","getBookingDetails")
+                .build();
     }
 
 //    @Autowired
